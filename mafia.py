0397706b7e038695be61b034718c1c168f03af6f
mhosseins1387 <mhosseins182@gmail.com>
2025-03-21 01:47:02 +0330

    takmil kar player ha dar MAFIA.PY

--- mafia.py
+++ mafia.py
@@ -5,11 +5,20 @@ print('slm . be mafia khoosh amadid . ma dar in bazi 5 najsh darim:1.shahvand , 
 time.sleep(4)
 a = int(input('tedad bazikonan : '))
 b = int(input('tedad mafia : '))
-c = a - b
+if a//2  <= b:
+    print('NEMISHE . MAFIA ZIADE!!!')
+    exit()
+c = a - b#city
+# taking player's names
 players = []
-for i in range(a):
-    x = input(f'bazikon shomare {i +1} : ')
+for i in range(1,1+a):
+    x = input(f'bazikon shomare {i} : ')
     players.append(x)
-print(players)
+# checking player's names
+for i in range(len(players)-1):
+    for x in range(i+1,len(players)):
+        if players[i]==players[x]:
+            exit(f'NEMISHE . esm bazikon {i+1} ba bazikon {x+1} yekie...')
+    
 
 
