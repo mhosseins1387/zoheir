8ebee004f4eb0f66cb1acd7b78b0f03c81d76cae
mhosseins1387 <mhosseins182@gmail.com>
2025-04-01 01:02:25 +0330

    mafia rules added...

--- d.py
+++ d.py
@@ -7,15 +7,15 @@ y = BeautifulSoup(x.text , "html.parser")
 def printTo(x):
  if 1<=x<=50 :
  	print('(healthy)')
- elif 51<=x<=100 : 
+ if 51<=x<=100 : 
  	print('(acceptable)')
- elif 101<=x<=150 :
+ if 101<=x<=150 :
  	print('(unhealthy<for a special group>)')
- elif 151<=x<=200 :
+ if 151<=x<=200 :
  	print('(unleahthy)')
- elif 200<=x<=251 :
+ if 200<=x<=251 :
  	print('(very unhealthy)')
- elif x >= 251  :
+ if x >= 251  :
  	print('(dangerous)')
 x = y.find(['span'], id="ContentPlaceHolder1_lblAqi3h")
 print(f'your air 1polution rate is at {x.string} ',end='')
--- mafia.py
+++ mafia.py
@@ -1,24 +1,51 @@
 import time
 
 
-print('slm . be mafia khoosh amadid . ma dar in bazi 5 najsh darim:1.shahvand , 2:mafia , 3:karagah , 4:doctor , 5:taktir andaz')
+print('slm . be mafia khoosh amadid . ma dar in bazi 5 nagsh darim:1.shahvand , 2:mafia , 3:karagah , 4:doctor , 5:taktir andaz')
 time.sleep(4)
 a = int(input('tedad bazikonan : '))
+if a <= 5 :
+    exit('NEMISHE . tedad player ha khayli baray 5 nash darid...')
+elif a > 12 :
+    exit('NEMISHE . playera ziyadan . had aksar 12 player...')
 b = int(input('tedad mafia : '))
-if a//2  <= b:
+if a//3  < b:
     print('NEMISHE . MAFIA ZIADE!!!')
     exit()
 c = a - b#city
+
+
 # taking player's names
 players = []
 for i in range(1,1+a):
     x = input(f'bazikon shomare {i} : ')
     players.append(x)
+
+
 # checking player's names
 for i in range(len(players)-1):
     for x in range(i+1,len(players)):
         if players[i]==players[x]:
             exit(f'NEMISHE . esm bazikon {i+1} ba bazikon {x+1} yekie...')
-    
+time.sleep(0.7)
+print('khayli khoob . hala vassta nagsh haro pakhsh konam...')
 
 
+#generating random numbers
+ran = []
+r_1 = int(time.time())%10
+while r_1 > a - 1:
+    r_1 = r_1 - 1
+ran.append(r_1)
+time.sleep(3.5)
+for i in range(b-1):
+    r_2 = int(time.time())%10
+    if r_2 > a-1 :
+        r_2 = r_1 - 2
+    ran.append(r_2)
+    time.sleep(1.75)
+print(ran)
+mafia = []
+for i in (ran):
+    mafia.append(players[i])
+print(mafia)
