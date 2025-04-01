2b5a5e3690a1ba7c84eb6e8b8a6b517a2783e0db
mhosseins1387 <mhosseins182@gmail.com>
2025-04-02 02:53:19 +0330

    all preperations for the mafia game is ready

--- mafia.py
+++ mafia.py
@@ -1,10 +1,15 @@
 import time
+import random
 
 
-print('slm . be mafia khoosh amadid . ma dar in bazi 5 nagsh darim:1.shahvand , 2:mafia , 3:karagah , 4:doctor , 5:taktir andaz')
-time.sleep(4)
+print('slm . be mafia khoosh amadid . ma dar in bazi 5 nagsh darim:1.shahvand , 2:mafia , 3:karagah , 4:doctor , 5:tak tir andaz')
+time.sleep(2.25)
+print('dar in bazi shoma mitavanid az 5 nafar bazikon ya bishtar estefade konid va tedad mafia ha bishtar az 1 nafar bashad')
+time.sleep(2.75)
+print('hala lotfan tedad bazikonan ra vared konid...')
+time.sleep(1.75)
 a = int(input('tedad bazikonan : '))
-if a <= 5 :
+if a < 5 :
     exit('NEMISHE . tedad player ha khayli baray 5 nash darid...')
 elif a > 12 :
     exit('NEMISHE . playera ziyadan . had aksar 12 player...')
@@ -31,21 +36,57 @@ time.sleep(0.7)
 print('khayli khoob . hala vassta nagsh haro pakhsh konam...')
 
 
-#generating random numbers
-ran = []
-r_1 = int(time.time())%10
-while r_1 > a - 1:
-    r_1 = r_1 - 1
-ran.append(r_1)
-time.sleep(3.5)
-for i in range(b-1):
-    r_2 = int(time.time())%10
-    if r_2 > a-1 :
-        r_2 = r_1 - 2
-    ran.append(r_2)
-    time.sleep(1.75)
-print(ran)
+# intruducing the roles
 mafia = []
-for i in (ran):
-    mafia.append(players[i])
+only_city = []
+city_all = []
+city = []
+karagah = []
+doctor = []
+tak_tir = []
+for i in range(a):
+    city.append(players[i])
+for i in range(a):
+    city_all.append(players[i])
+for i in range(a):
+    only_city.append(players[i])
+
+
+
+
+#generating the roles
+for i in range(b):
+    x = random.choice(players)
+    mafia.append(x)
+    city.remove(x)
+    only_city.remove(x)
+
+rand = []
+for i in range(3):
+    x = random.choice(city)
+    city.remove(x)
+    rand.append(x)
+karagah.append(rand[0])
+doctor.append(rand[1])
+tak_tir.append(rand[2])
+
+
+# showing the roles
+print('khayli khoob . hala nagsh ha pakhsh mishavand...')
+print('lotfan ravi bazi nagsh ra bebinid va be bazikonan nagsh hayshan ra begin...')
+time.sleep(2)
+print('mafia ha :')
 print(mafia)
+time.sleep(2)
+print('karagah :')
+print(karagah)
+time.sleep(2)
+print('doctor :')
+print(doctor)
+time.sleep(2)
+print('tak tir andaz :')
+print(tak_tir)
+time.sleep(2)
+print('tamami shahvand ha :')
+print(city_all)
+time.sleep(2)
--- t.py
+++
@@ -1,3 +0,0 @@
-for i in range(5):
-    for x in range(5):
-        print(i,x)
