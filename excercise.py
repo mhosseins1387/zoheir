acd6a50410b4afd65c2fa40cf75ac07566cd111c
mhosseins1387 <mhosseins182@gmail.com>
2025-04-05 23:15:49 +0330

    c

--- excercise.py
+++ excercise.py
@@ -1,12 +1,15 @@
-# n = int(input())
-# list = []
-# x = 1
-# for i in range(1,n+1):
-#     list.append(i)
-# for i in range(len(list)):
-#     x = x*list[i]
-# print(x) 
-l = [1,2,3]
-i = [1,2,3]
-if l == i:
-    print('yes')
+# # n = int(input())
+# # list = []
+# # x = 1
+# # for i in range(1,n+1):
+# #     list.append(i)
+# # for i in range(len(list)):
+# #     x = x*list[i]
+# # print(x) 
+# l = [1,2,3]
+# i = [1,2,3]
+# if l == i:
+#     print('yes')
+l = []
+if l == []:
+    print(1)
--- mafia.py
+++ mafia.py
@@ -1,5 +1,6 @@
 import time
 import random
+from time import sleep as deley
 
 
 print('slm . be mafia khoosh amadid . ma dar in bazi 5 nagsh darim:1.shahvand , 2:mafia , 3:karagah , 4:doctor , 5:tak tir andaz')
@@ -38,6 +39,7 @@ time.sleep(0.7)
 # intruducing the roles
 mafia = []
 only_city = []
+only_CITY = []
 city_all = []
 city = []
 karagah = []
@@ -49,8 +51,8 @@ for i in range(a):
     city_all.append(players[i])
 for i in range(a):
     only_city.append(players[i])
-
-
+for i in range(a):
+    only_CITY.append(only_city[i])
 
 
 #generating the roles
@@ -103,6 +105,7 @@ city_death = []
 karagah_death = []
 doctor_death = []
 tak_tir_death = []
+doctor_choices = []
 
 
 #the first night
@@ -119,14 +122,14 @@ night += 1
 
 # the first day
 print(f'sobh aval :')
-time.sleep(1)
+time.sleep(3)
 print('shahvand ha bidar shavand va be modat 15 sanie ba hamdigar ashna mishavand...')
 time.sleep(6)
 for i in range(len(players)):
     print(f'bazikon shomare {i + 1} : 15 sanie sohbat darad...')
     time.sleep(0)
     print('vaght tamam shod...')
-    time.sleep(2)
+    time.sleep(0)
 print('rooz aval tamam shod . hala shab dovom shoroo mishavad...')
 time.sleep(2)
 day += 1
@@ -134,24 +137,51 @@ day += 1
 
 # game loop
 while True:
+# night
 
-
-    # night
+    # mafia
     print(f'shabe {night} :')
     time.sleep(1)
     print('mafia ha bidar shavand va az mian shahrvand ha yeki ra bayar koshtan entekhab konnand...')
     print(only_city)
     time.sleep(2)
     kill_choice = input('esm bazikoni ra ke mikhahid bokoshid vared konid : ')
-    while kill_choice not in only_city:
-        kill_choice = input('esm bazikon ra DOROST vared konid : ')
-    while kill_choice in mafia:
+    while kill_choice not in only_CITY:
+        kill_choice = input('esm bazikon eshtebah vared shode . esm bazikon ra DOROST vared konid : ')
+    while kill_choice in mafia or mafia_death:
         kill_choice = input('mafia nemitavanad khodash ra bokoshad . lotfan yek shahrvand ra entekhab konid : ')
-    while kill_choice in death:
+    while kill_choice in death or city_death or karagah_death or doctor_death or tak_tir_death:
         kill_choice = input('in shahrvand ghablan koshte shode ast . lotfan yek shahrvand digar ra entekhab konid : ')
+    time.sleep(0.5)
     death.append(kill_choice)
-    city_death.append(kill_choice)
     only_city.remove(kill_choice)
-    time.sleep(2)
+    if kill_choice in only_city:
+        city_death.append(kill_choice)
+    if kill_choice in karagah:
+        karagah_death.append(kill_choice)
+    if kill_choice in doctor:
+        doctor_death.append(kill_choice)
+    if kill_choice in tak_tir:
+        tak_tir_death.append(kill_choice)
+    print(f'{kill_choice} koshte shod ... mafia ha bekhaban.')
+    time.sleep(6.125)
+    # karagah
+    print('karagah bidar she')
+    deley(1.25)
     print(only_city)
-    print(death)
+    estelam = input('lotfan esm fardi ke mikhahid estelam begirid vared konid : ')
+    while estelam not in players:
+        estelam = input('esm fard eshtebah bood . esm dorost ra vared konid : ')
+    while estelam in death:
+        estelam = input('in fard khoshte shode ast . lotfan fard digari ra entekhab konid : ')
+    if estelam in mafia :
+        print(f'{estelam} MAFIA hast...')  
+    else:
+        print(f'{estelam} mafia NIST...')
+    deley(2.5)
+    print('lotfan bekhabid....')
+    # doctor
+    print('doctor bidar she')
+    deley(2)
+     
+
